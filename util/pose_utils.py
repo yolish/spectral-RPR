3f28f10cd36cd91b9f6dee57746b08a1399b97c5
ronferens <ronferens@gmail.com>
2021-05-03 14:29:27 +0300

    Updated inverse translation operation

--- util/pose_utils.py
+++ util/pose_utils.py
@@ -5,7 +5,7 @@ def calc_relative_rot_mat(rot_mat1, rot_mat2):
     """
     Calculates the relative rotation matrix
     """
-    rot_mat2_inv = rot_mat2.T#np.linalg.inv(rot_mat2)
+    rot_mat2_inv = np.linalg.inv(rot_mat2)
     rel_rot_mat_12 = np.dot(rot_mat1, rot_mat2_inv)
     return rel_rot_mat_12
 
